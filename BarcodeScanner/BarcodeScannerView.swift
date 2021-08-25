2c86d5aecce7c3c50c7e9fea89b29b0254e48126
Ryan Perez <ryancs115@gmail.com>
2021-08-24 20:25:38 -0700

    setup scanner layout design

--- BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -13,6 +13,18 @@ struct BarcodeScannerView: View {
             VStack {
                 Rectangle()
                     .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer().frame(height: 60)
+                
+                Label("Scanned Barcode:", systemImage:
+                      "barcode.viewfinder")
+                    .font(.title)
+                
+                Text("Not Yet Scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
             }
             .navigationTitle("Barcode Scanner")
         }
