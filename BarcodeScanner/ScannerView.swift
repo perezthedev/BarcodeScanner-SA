149f1eb64218f136666239fbb01e28f87663b009
Ryan Perez <ryancs115@gmail.com>
2021-08-29 09:29:29 -0700

    coordinator listening for ScannerVC

--- BarcodeScanner/ScannerView.swift
+++ BarcodeScanner/ScannerView.swift
@@ -8,21 +8,26 @@
 import SwiftUI
 
 struct ScannerView: UIViewControllerRepresentable {
+    
     func makeUIViewController(context: Context) -> ScannerVC {
         ScannerVC(scannerDelegate: context.coordinator)
     }
     
     func updateUIViewController(_ uiViewController: ScannerVC, context: Context) {}
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
+    
     final class Coordinator: NSObject, ScannerVCDelegate {
         func didFind(barcode: String) {
-            <#code#>
+            print(barcode)
         }
         
         func didSurface(error: CameraError) {
-            <#code#>
+            print(error.rawValue)
         }
-        
         
     }
 }
