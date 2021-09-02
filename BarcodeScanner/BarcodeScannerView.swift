a14acef77d9d64f8858543a9743ed426d3e49d6f
Ryan Perez <ryancs115@gmail.com>
2021-09-01 21:20:45 -0700

    added scannedCode ternary operators

--- BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -2,12 +2,15 @@
 //  ContentView.swift
 //  BarcodeScanner
 //
-//  Created by thepercussivedev on 8/24/21.
+//  Created by PerezTheDev on 8/24/21.
 //
 
 import SwiftUI
 
 struct BarcodeScannerView: View {
+    
+    @State private var scannedCode = ""
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -20,10 +23,10 @@ struct BarcodeScannerView: View {
                       "barcode.viewfinder")
                     .font(.title)
                 
-                Text("Not Yet Scanned")
+                Text(scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode)
                     .bold()
                     .font(.largeTitle)
-                    .foregroundColor(.green)
+                    .foregroundColor(scannedCode.isEmpty ? .red : .green)
                     .padding()
             }
             .navigationTitle("Barcode Scanner")
