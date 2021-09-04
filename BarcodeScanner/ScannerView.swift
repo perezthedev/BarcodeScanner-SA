44b852a20ed223775f75858645716dd0f95e6723
Ryan Perez <ryancs115@gmail.com>
2021-09-03 23:06:50 -0700

    scannedCode property passed through didFind method

--- BarcodeScanner/ScannerView.swift
+++ BarcodeScanner/ScannerView.swift
@@ -18,13 +18,18 @@ struct ScannerView: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: ScannerVC, context: Context) {}
     
     func makeCoordinator() -> Coordinator {
-        Coordinator()
+        Coordinator(scannerView: self)
     }
     
-    
     final class Coordinator: NSObject, ScannerVCDelegate {
+        private let scannerView: ScannerView
+        
+        init(scannerView: ScannerView) {
+            self.scannerView = scannerView
+        }
+        
         func didFind(barcode: String) {
-            print(barcode)
+            scannerView.scannedCode =  barcode
         }
         
         func didSurface(error: CameraError) {
